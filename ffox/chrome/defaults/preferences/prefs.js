830441927b82e418ebecf700ebf968b976295719
Constantine de Castro <constantine.decastro@gmail.com>
2012-03-05 01:33:35 -0500

    Firefox v1.92
    Cleanup.

--- ffox/chrome/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.rchesspgnviewer.boolpref", false);
-pref("extensions.rchesspgnviewer.intpref", 0);
-pref("extensions.rchesspgnviewer.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.rchesspgnviewer@constantinedecastro.com.description", "chrome://rchesspgnviewer/locale/overlay.properties");
