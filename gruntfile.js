6c531d991340844563d0cc942290e377d16f9653
Dalton Woods <dalton.c.woods@gmail.com>
2015-07-25 13:07:12 -0400

    Add watch and shell tasks for building the Firefox add-on.

--- gruntfile.js
+++ gruntfile.js
@@ -7,10 +7,21 @@ module.exports = function(grunt){
           jQuery : true
         }
       }
+    },
+    shell: {
+      buildFirefox: {
+        command: 'cd ffox && jpm run'
+      }
+    },
+    watch: {
+      files: ['chrome/*.js', 'chrome/*.css'],
+      tasks: ['shell:buildFirefox']
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-shell");
 
   grunt.registerTask("default", ["jshint"]);
 }
