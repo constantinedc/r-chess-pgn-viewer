eb40eb2da37b1367f334091c3efb0173dcca3fa6
Constantine de Castro <tingster.ex@gmail.com>
2015-07-27 20:46:02 -0400

    Finished plugging in all the basic grunt tasks.
    Also added grunt-contrib-clean as a dependency.
    Also changed chrome to use jquery 2.1.4 min, matching the same one firefox was using.

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,38 @@
 module.exports = function(grunt){
   grunt.initConfig({
+    copy: {
+      chrome: {
+        files: [
+          {expand: true, flatten: true, src: ['core/*'], dest: 'chrome/'},
+          {expand: true, flatten: true, src: ['viewer/*'], dest: 'chrome/'},
+        ]
+      },
+      firefox: {
+        files: [
+          {expand: true, flatten: true, src: ['core/*'], dest: 'firefox/data/'},
+          {expand: true, flatten: true, src: ['viewer/*'], dest: 'firefox/data/'},
+        ]
+      }
+    },
+
+    watch: {
+      chrome: {
+        files: ['core/*', 'viewer/*'],
+        tasks: ['copy:chrome']
+      },
+      firefox: {
+        files: ['core/*', 'viewer/*'],
+        tasks: ['copy:firefox']
+      }
+    },
+
+    clean: {
+      chrome: ["chrome/*", "!chrome/manifest.json"],
+      firefox: ["firefox/data/*"]
+    },
+
     jshint: {
-      files: ["chrome/rchesspgn.js"],
+      files: ["core/rchesspgn.js"],
       options : {
         globals: {
           jQuery : true
@@ -11,6 +42,14 @@ module.exports = function(grunt){
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-clean");
 
-  grunt.registerTask("default", ["jshint"]);
+  grunt.registerTask("default", ["copy"]);
+
+  grunt.registerTask('chrome', ['copy:chrome', 'watch:chrome']);
+  grunt.registerTask('firefox', ['copy:firefox', 'watch:firefox']);
+
+  grunt.registerTask("package", ["jshint"]);//stub for other package tasks.
 }
